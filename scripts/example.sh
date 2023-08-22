ae8547bef4daa8dc8961258a8c1053b7403c49cc
Authomats <th0m4z2000@gmail.com>
2023-08-23 00:46:29 +0200

    commiting murder

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
 
+#clang -c ../asm/example.S -o ../temp/example.o
+#clang ../temp/example.o -o ../bin/example
 
-gcc -c ../asm/example.S -o ../temp/example.o
-gcc ../temp/example.o -o ../bin/example
+fmt=""
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # Linux
+        fmt="-felf64"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        fmt="-fmacho64"
+fi
+
+yasm --parser="gas" $fmt ../asm/example.S -o ../temp/example.o
+clang ../temp/example.o -o ../bin/example
 cd ../bin
 ./example
